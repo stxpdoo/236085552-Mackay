6291f4ff7a051b8667da230e7f225e1dcab51aa7
S312User <S312User@S312.uc-bcf.edu.ph>
2025-03-12 15:17:19 +0800

    Adding new php folder and contents

---
+++ phpIntro/phptestcode.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Example</title>
+</head>
+<body>
+    <?php
+
+    echo "<h1>Exercise 1: Exercise 1: Variables and Operators</h1>";
+    echo "<h3>a. Declare two variables, a and b, with values 15 and 7.</h2>";
+    echo "<h3>b. Compute and print their sum, difference, product, and quotient.</h2>";
+        $a = 15;
+        $b = 7;
+    echo "<p>if A = " . ($a) . " and b = " . ($b) . "</p>";
+    echo "<p>Sum: " . ($a + $b) . "</p>";
+    echo "<p>Difference: " . ($a - $b) . "</p>";
+    echo "<p>Product: " . ($a * $b) . "</p>";
+    echo "<p>Quotient: " . ($a / $b) . "</p>";
+    echo "<br>";
+
+    echo "<h1>Exercise 2: Conditional Statements</h1>";
+    echo "<h3>a. Write a PHP program that checks if a number is even or odd.</h3>";
+    echo "<h3>b. Modify the program to check if the number is positive, negative, or zero.</h3>";
+    
+
+
+
+    ?>
+</body>
+</html>
